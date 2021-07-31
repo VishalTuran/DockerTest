04e49c89af931a4a183a3be28f9b6c758534f96c
Vishal Turan <vturan@VTuran1M-BNG1.local>
2021-07-31 22:53:53 +0530

    Updated Zelenium

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,38 +1,67 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SampleTest {
     WebDriver driver;
     @BeforeClass
-    public void setUp(){
-        String dir=System.getProperty("user.dir");
-        String driverLocation = dir + File.separatorChar + "src" + File.separatorChar + "main" + File.separatorChar + "java" + File.separatorChar + "chromedriver_linux";
-        System.setProperty("webdriver.chrome.driver", driverLocation);
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("test-type");
-        options.addArguments("--window-size=1400,600");
-        options.addArguments("--disable-extensions");
-         options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("start-maximized");
-        System.setProperty("webdriver.chrome.whitelistedIps", "");
-        driver = new ChromeDriver(options);
-
+    public void setUp() throws MalformedURLException {
+        // String dir=System.getProperty("user.dir");
+        // String driverLocation = dir + File.separatorChar + "src" + File.separatorChar + "main" + File.separatorChar + "java" + File.separatorChar + "chromedriver_linux";
+        // System.setProperty("webdriver.chrome.driver", driverLocation);
+        // ChromeOptions options = new ChromeOptions();
+        // options.addArguments("test-type");
+        // options.addArguments("--window-size=1400,600");
+        // options.addArguments("--disable-extensions");
+        //  options.addArguments("--headless");
+        // options.addArguments("--no-sandbox");
+        // options.addArguments("start-maximized");
+        // System.setProperty("webdriver.chrome.whitelistedIps", "");
+        // driver = new ChromeDriver(options);
+        DesiredCapabilities desiredCapabilities= DesiredCapabilities.chrome();
+        driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),desiredCapabilities);
         driver.manage().window().maximize();
     }
+
+    @Test
+    public void assertGoogleTitle2(){
+        driver.get("https://www.google.com");
+        Assert.assertTrue(driver.getTitle().contains("Google"),"title mismatched");
+    }
+
+    @Test
+    public void assertGoogleTitle3(){
+        driver.get("https://www.google.com");
+        Assert.assertTrue(driver.getTitle().contains("Google"),"title mismatched");
+    }
+
+    @Test
+    public void assertGoogleTitle4(){
+        driver.get("https://www.google.com");
+        Assert.assertTrue(driver.getTitle().contains("Google"),"title mismatched");
+    }
+
+    @Test
+    public void assertGoogleTitle5(){
+        driver.get("https://www.google.com");
+        Assert.assertTrue(driver.getTitle().contains("Google"),"title mismatched");
+    }
+
     @Test
     public void assertGoogleTitle(){
         driver.get("https://www.google.com");
         Assert.assertTrue(driver.getTitle().contains("Google"),"title mismatched");
     }
-
     @AfterClass
     public void tearDown(){
         driver.quit();
